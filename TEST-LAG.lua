38a78b3ee0cebe5d1e8738e744291319e4c2e998
Loader-Desconhecido <luisdozero82@gmail.com>
2026-01-02 21:44:52 -0400

    Update TEST-LAG.lua

--- TEST-LAG.lua
+++ TEST-LAG.lua
@@ -22,7 +22,7 @@ local Camera = workspace.CurrentCamera
 
 local STREAM_RADIUS = 390
 local GRAPHICS_LEVEL = 1
-local SOUND_LIMIT = 0.9
+local SOUND_LIMIT = 0.6
 
 --==============================
 -- GRÁFICO FORÇADO NO 1
